387c2d85d731fd42820f3ab5119b6e7f522809e3
Shuwax <spike123on@gmial.com>
2017-10-16 16:11:30 +0200

    Datatable edit date

--- app/Http/Controllers/WorkHoursController.php
+++ app/Http/Controllers/WorkHoursController.php
@@ -29,9 +29,20 @@ class WorkHoursController extends Controller
 
             $start_date = $request->start_date;
             $stop_date = $request->stop_date;
-            $query = Work_Hour::select('id', 'click_start')
+            $query = DB::table('work_hours')
+                ->join('users', 'work_hours.id_user', '=', 'users.id')
+                ->select(DB::raw(
+                    'work_hours.id as id,
+                    users.first_name,
+                    users.last_name,
+                    work_hours.click_start,
+                    work_hours.click_stop,
+                    work_hours.register_start,
+                    work_hours.register_stop,
+                    work_hours.date,
+                    SEC_TO_TIME(TIME_TO_SEC(register_stop) - TIME_TO_SEC(register_start) ) as time'))
+                ->where('work_hours.status', '=', 2)
                 ->whereBetween('date',[$start_date,$stop_date]);
-
             return datatables($query)->make(true);
 
         }
--- resources/views/workhours/acceptHour.blade.php
+++ resources/views/workhours/acceptHour.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.main')
+@section('style')
+    <style>
+        td{
+            text-align: center;
+        }
+
+        #register_stop
+        {
+            float: left;
+            width: 75px;
+            height: 28px;
+            margin-top: 3px;
+        }
+        #register_start
+        {
+            width: 75px;}
+
+    </style>
+@endsection
 @section('content')
 
 
@@ -14,7 +33,7 @@
         <div class="form-group">
             <label for ="ipadress">Od:</label>
             <div class="input-group date form_date col-md-5" data-date="" data-date-format="yyyy-mm-dd" data-link-field="datak" style="width:100%;">
-                <input id="start_date" class="form-control" name="od" type="text" value="{{date("Y-m-d")}}" readonly >
+                <input  onchange="myFunction()"  id="start_date" class="form-control" name="od" type="text" value="{{date("Y-m-d")}}" readonly >
 
                 <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
             </div>
@@ -22,7 +41,7 @@
         <div class="form-group">
             <label for ="ipadress">Do:</label>
             <div class="input-group date form_date col-md-5" data-date="" data-date-format="yyyy-mm-dd" data-link-field="datak" style="width:100%;">
-                <input id="stop_date" class="form-control" name="do" type="text" value="{{date("Y-m-d")}}"readonly >
+                <input onchange="myFunction()" id="stop_date" class="form-control" name="do" type="text" value="{{date("Y-m-d")}}"readonly >
 
                 <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
             </div>
@@ -31,8 +50,13 @@
     <table id="datatable">
         <thead>
         <tr>
-            <th>id</th>
-            <th>click_start</th>
+            <th>Data</th>
+            <th>Osoba</th>
+            <th>Start</th>
+            <th>Zarejestrowane</th>
+            <th>Modyfikacja</th>
+            <th>Suma</th>
+            <th>Akcja</th>
         </tr>
         </thead>
         <tbody>
@@ -41,6 +65,7 @@
 
     <div class="row">
         <div class="col-lg-12">
+            </br> <span class="fa fa-user fa-fw"></span> </br>
         </div>
     </div>
 
@@ -50,7 +75,12 @@
 @section('script')
 
     <script>
+        var table;
 
+
+        function myFunction() {
+            table.ajax.reload();
+        }
         $(function() {
             $('.form_date').datetimepicker({
                 language:  'pl',
@@ -58,9 +88,21 @@
                 minView : 2,
                 pickTime: false,
             });
+                $('.form_time').datetimepicker({
+                    language:  'pl',
+                    weekStart: 1,
+                    todayBtn:  1,
+                    autoclose: 1,
+                    todayHighlight: 1,
+                    startView: 1,
+                    minView: 0,
+                    maxView: 1,
+                    forceParse: 0
+                });
         });
+
         $(document).ready( function () {
-            $('#datatable').DataTable({
+            table = $('#datatable').DataTable({
                 "processing": true,
                 "serverSide": true,
                 "ajax": {
@@ -76,11 +118,72 @@
                     "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Polish.json"
                 },
                 "columns":[
-                    {"data": "id"},
-                    {"data": "click_start"}
-                ]
-            })
-        })
+                    {"data": "date"},
+
+                    {"data":function (data, type, dataToSet) {
+                        return data.first_name + " " + data.last_name;
+                    },"name": "users.last_name"},
+
+                    {"data": function (data, type, dataToSet) {
+                        if(data.click_start == null)
+                            data.click_start = "Brak infromacji";
+                        if(data.click_stop == null)
+                            data.click_stop = "Brak infromacji";
+                        return data.click_start + "</br><span class='fa fa-arrow-circle-o-down fa-fw'></span> </br> " + data.click_stop;
+                    },"name": "click_start" },
+
+                    {"data": function (data, type, dataToSet) {
+                        if(data.register_start == null)
+                            data.register_start = "Brak infromacji";
+                        if(data.register_stop == null)
+                            data.register_stop = "Brak infromacji";
+                        return data.register_start + "</br><span class='fa fa-arrow-circle-o-down fa-fw'></span> </br> " + data.register_stop;
+                    },"name": "register_start"},
+
+                    {"data":null,"targets": -3,"orderable": false, "searchable": false },
+
+                    {"data": "time", "name": "time","searchable": false },
+
+                    {"data":null,"targets": -1,"orderable": false, "searchable": false }
+
+                ],
+                "columnDefs": [ {
+                    "targets": -1,
+                    "data": null,
+                    "defaultContent": "<button id='zapis'>Zapisz</button>"
+                },
+                    {
+                        "targets": -3,
+                        "data": null,
+                        "defaultContent": "" +
+                        "<div class='form-group'>" +
+                        "<div class='input-group date form_time col-md-5' data-date='' data-date-format=hh:ii' data-link-field='dtp_input3' data-link-format='hh:ii'>"+
+                        "<input id='register_start' class='form-control' size='16' type='text' value='' readonly>"+
+                        "<span class='input-group-addon'><span class='glyphicon glyphicon-remove'></span></span>"+
+                        "<span class='input-group-addon'><span class='glyphicon glyphicon-time'></span></span>"+
+                        "</div>"+
+                        "<input type='hidden' id='dtp_input3' value='' /><br/>"+
+                        "</div>"
+                    }]
+            });
+            $('#datatable tbody').on( 'click', '#register_start', function () {
+                $("#success-alert").alert();
+                $('.form_time').datetimepicker({
+                    language:  'pl',
+                    weekStart: 1,
+                    todayBtn:  1,
+                    autoclose: 1,
+                    todayHighlight: 1,
+                    startView: 1,
+                    minView: 0,
+                    maxView: 1,
+                    forceParse: 0
+                }).focus();
+            });
+
+        });
+
+
 
 
     </script>
@@ -99,5 +202,4 @@
 
 
 
-    </script>
 @endsection
