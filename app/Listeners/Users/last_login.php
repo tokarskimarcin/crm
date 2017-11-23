6f372647c0899445039e4e05ecfdcf70b21c7ec7
Sebastian Cytawa <spike123on@gmail.com>
2017-11-23 13:56:02 +0100

    Last login option

---
+++ app/Listeners/Users/last_login.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners\Users;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class last_login
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $event->user->last_login = $today = date("Y-m-d");
+        $event->user->save();
+    }
+}
