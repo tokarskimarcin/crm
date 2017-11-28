5a2311be63525ac8d63235b3ed2bf998d9dd2192
Sebastian Cytawa <spike123on@gmail.com>
2017-11-28 13:24:53 +0100

    Add Hour Report 50%

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\HourReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StatisticsController extends Controller
 {
     public function hourReportGet()
     {
-        return view('statistics.hourReport');
+        $reports = HourReport::where('report_date','like','2017-11-28')
+            ->where('department_info_id',Auth::user()->department_info_id)->get();
+        return view('statistics.hourReport')
+            ->with('reports',$reports);
     }
     public function hourReportPost(Request $request)
     {
--- resources/views/statistics/hourReport.blade.php
+++ resources/views/statistics/hourReport.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <style>
-    button{
+    #send_button{
         width: 100%;
         height: 33px;
         margin-top: 25px;
@@ -29,10 +29,11 @@
                             <div id="start_stop">
                                 <div class="well">
                                 <div class="panel-body">
-                                            <form method="post">
+                                            <form method="post" action="hour_report">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">Godzina:</label>
-                                                    <select name="raportgodzinny_hour" class="form-control" style="font-size:18px;">
+                                                    <label>Godzina:</label>
+                                                    <select name="hour" class="form-control" style="font-size:18px;">
                                                         <option>Wybierz</option>
                                                         @for ($i=9; $i < 22; $i++)
                                                             @php($godz = $i.':00')
@@ -53,34 +54,34 @@
                                                 </div>
 
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">Średnia:</label>
-                                                    <input class="form-control numeric" name="raportgodzinny_average" type="text" value="">
+                                                    <label>Średnia:</label>
+                                                    <input class="form-control numeric" name="average" type="text">
                                                 </div>
 
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">Liczba Zaproszeń:</label>
-                                                    <input class="form-control" name="raportgodzinny_success" type="number" value="">
+                                                    <label>Liczba Zaproszeń:</label>
+                                                    <input class="form-control" name="success" type="number" value="">
                                                 </div>
 
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">Liczba Osób:</label>
-                                                    <input class="form-control" name="raportgodzinny_users" type="text" value="">
+                                                    <label>Liczba Osób:</label>
+                                                    <input class="form-control" name="employee_count" type="text" value="">
                                                 </div>
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">% Janków:</label>
-                                                    <input class="form-control" name="raportgodzinny_procjan" type="text" value="">
+                                                    <label>% Janków:</label>
+                                                    <input class="form-control" name="janky_count" type="text" value="">
                                                 </div>
 
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">% Wykorzystania Bazy</label>
-                                                    <input class="form-control" name="raportgodzinny_zb" type="text" value="">
+                                                    <label>% Wykorzystania Bazy</label>
+                                                    <input class="form-control" name="wear_base" type="text" value="">
                                                 </div>
                                                 <div class="col-md-3">
-                                                    <label for="exampleInputPassword1">Czas Rozmów:</label>
-                                                    <input class="form-control" name="raportgodzinny_call_time" type="text" value=""></br>
+                                                    <label>Czas Rozmów:</label>
+                                                    <input class="form-control" name="call_Time" type="text" value=""></br>
                                                 </div>
                                                 <div class="col-md-3">
-                                                    <button type="submit" class="btn btn-primary" name="raportgodzinny_send" >Wyślij</button>
+                                                    <button type="submit" class="btn btn-primary" name="hour_report_send" id="send_button">Wyślij</button>
                                                 </div>
                                             </form>
                                         </div>
@@ -109,15 +110,55 @@
                                 <th align="center">% Wykorzystania Bazy</th>
                                 <th align="center">Czas Rozmów</th>
                                 <th align="center">% Celu</th>
+                                <th align="center">Status</th>
+                                <th align="center">Akcja</th>
                             </tr>
                             </thead>
                             @for ($i=9; $i < 22; $i++)
+                                @php($is_set = 0)
                                 <tr>
                                 @php($godz = $i.':00')
                                 @if ($godz == '9:00')
                                     @php($godz = '09:00')
                                 @endif
-                                <td>{{$godz}}</td>
+                                    <td>{{$godz}}</td>
+                                    @foreach($reports as $report)
+                                        @if($report->hour ==  $godz.':00')
+                                            @php($is_set = 1)
+                                            <td>{{$report->average}}</td>
+                                            <td>{{$report->success}}</td>
+                                            <td>{{$report->employee_count}}</td>
+                                            <td>{{$report->janky_count}} %</td>
+                                            <td>{{$report->wear_base}} %</td>
+                                            <td>{{$report->call_time}} %</td>
+                                            @if(date('N', strtotime(date('Y-m-d'))) >= 6)
+                                                <td>{{round(($report->success*100)/$report->department_info->dep_aim_week,2)}}</td>
+                                            @else
+                                                <td>{{round(($report->success*100)/$report->department_info->dep_aim,2)}}</td>
+                                            @endif
+                                            <td id="status_{{$i}}">{{ $report->is_send == 1 ? "Wysłany" : "Oczekuje na wysłanie" }}</td>
+                                            @if($report->is_send == 1)
+                                            <td>
+                                                <button name={{$i}} class="btn btn-primary disabled">Edycja</button>
+                                            </td>
+                                            @else
+                                                <td>
+                                                    <button name={{$i}} class="btn btn-primary active" type="button" data-toggle="modal" data-target="#edit" id={{$report->id}}>Edycja</button>
+                                                </td>
+                                            @endif
+                                        @endif
+                                    @endforeach
+                                    @if($is_set == 0)
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    @endif
                                 </tr>
                             @endfor
                             <tbody>
@@ -128,14 +169,33 @@
             </div>
         </div>
     </div>
-
-
 @endsection
 
 @section('script')
-
 <script>
 
+    (function () {
+        function checkTime(i) {
+            return (i < 10) ? "0" + i : i;
+        }
+
+        function startTime() {
+            var today = new Date(),
+                h = checkTime(today.getHours()),
+                m = checkTime(today.getMinutes());
+            if(m>=8)
+            {
+                var button = document.getElementsByName(h).item(0);
+                document.getElementById('status_'+h).innerHTML = "Wysłany";
+                if(button != null)
+                    button.setAttribute('disabled', true);
+            }
+            t = setTimeout(function () {
+                startTime()
+            }, 5000);
+        }
+        startTime();
+    })();
 
 </script>
 @endsection
