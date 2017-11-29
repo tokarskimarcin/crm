0e2ed03311199366e1536739429edf232cb17788
Sebastian Cytawa <spike123on@gmail.com>
2017-11-29 08:52:45 +0100

    Fix Equipment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -14,4 +15,15 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         '/logout'
     ];
+    public function handle($request, Closure $next)
+    {
+        if (
+            parent::isReading($request) ||
+            parent::runningUnitTests() ||
+            parent::tokensMatch($request)
+        ) {
+            return parent::addCookieToResponse($request, $next($request));
+        }
+        return back()->with('error','The token has expired, please try again.');
+    }
 }
