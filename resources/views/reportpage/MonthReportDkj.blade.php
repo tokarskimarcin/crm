c178620484f61eabba352c33b69aa9bbae113cae
Sebastian Cytawa <spike123on@gmail.com>
2017-12-07 17:02:28 +0100

    Fix payment

--- resources/views/reportpage/MonthReportDkj.blade.php
+++ resources/views/reportpage/MonthReportDkj.blade.php
@@ -14,8 +14,8 @@
                         <div class="col-lg-12">
                             <div id="start_stop">
                                 <div class="panel-body">
-                                        @include('mail.weekReportDkj')
-                                    </div>
+                                    @include('mail.monthReportDkj')
+                                </div>
                                 </div>
                             </div>
                         </div>
