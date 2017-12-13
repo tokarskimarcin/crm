345a471264686c2489c95dbae25b79e10442fb96
Sebastian Cytawa <spike123on@gmail.com>
2017-12-13 13:17:40 +0100

    Fix view

--- resources/views/reportpage/WeekReportJanky.blade.php
+++ resources/views/reportpage/WeekReportJanky.blade.php
@@ -1,38 +1,25 @@
 @extends('layouts.main')
 @section('content')
 
+
     {{--Header page --}}
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Panel zarządzania</h1>
+            <h1 class="page-header">Raport Podważonych Janków</h1>
         </div>
     </div>
-
-
     <div class="row">
         <div class="col-lg-12">
-
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                   Rapotr tygodniowy podważonych janków
-                </div>
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div id="start_stop">
-                                <div class="panel-body">
-                                    <div class="well">
-                                          @include('mail.weekReportJanky')
-                                    </div>
-                                </div>
+                <div class="row">
+                            <div class="panel-body" style="font-size: 12px;">
+                                @include('mail.weekReportJanky')
                             </div>
-                        </div>
-                    </div>
                 </div>
-            </div>
         </div>
     </div>
     </div>
+
+
 
 @endsection
 
