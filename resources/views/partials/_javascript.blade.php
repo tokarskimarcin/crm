1e4f458f7e6a96cfe89da91eccd40f6556016a53
konradja100 <konradja100@wp.pl>
2017-12-14 14:00:54 +0100

    Bimbałek

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -99,9 +99,9 @@ $(function(){
 
 
                   } else { // tutaj jezeli nie ma oddział nie jest podzielony na badania/wysyłkę
-                      $("#" + response[i].department_info_id + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].yanky_count + ")");
-                      $("#" + response[i].department_info_id + "dkjstatus td[name='count_yanek']").text(response[i].bad);
-                      $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text(response[i].manager_disagreement);
+                      $("#" + response[i].department_info_id + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].all_check_talk + ")");
+                      $("#" + response[i].department_info_id + "dkjstatus td[name='count_yanek']").text(response[i].all_bad);
+                      $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text(response[i].manager_research_janky_count);
                       $("#" + response[i].department_info_id + "dkjstatus td[name='status']").removeClass("alert-danger");
                       $("#" + response[i].department_info_id + "dkjstatus td[name='status']").addClass("alert-success");
                   }
@@ -125,50 +125,64 @@ $(function(){
               'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
           },
           success: function(response) {
-              var sound = true;
+              var sound = false;
+							console.log(response);
               for (var i = 0; i < response.length; i++) {
 
-                  var countMe = $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text();
-                  if (countMe != response[i].manager_research_janky_count && sound == true) {
-                      //tutaj ding
-                      var snd = new Audio("{{asset('assets/1.mp3')}}");
-                      snd.play();
-                      sound = false;
-                      $('#check_messages_dkj').html('(!) <i class="fa fa-envelope fa-fw"></i><i class="fa fa-caret-down"></i>');
-                  }
+									if (response[i].type == 'Badania/Wysyłka') {
+											var countMeShipping = $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='undone']").text();
+											var countMeResearch = $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text();
+											if ((response[i].manager_research_janky_count != countMeResearch && response[i].manager_research_janky_count > countMeResearch)
+												|| response[i].manager_shipping_janky_count != countMeShipping && response[i].manager_shipping_janky_count > countMeShipping) {
+													var sound = true;
+											}
 
-
-                  $("#" + response[i].department_info_id + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].research_all + ")");
-                  $("#" + response[i].department_info_id + "dkjstatus td[name='count_yanek']").text(response[i].research_janky_count);
-                  $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text(response[i].manager_research_janky_count);
-                  $("#" + response[i].department_info_id + "dkjstatus td[name='status']").removeClass("alert-danger");
-                  $("#" + response[i].department_info_id + "dkjstatus td[name='status']").addClass("alert-success");
-
-                  //sprawdzenie obecnosci odrzuconego janka dla oddziałów na minusie
-                  i *= -1;
-                  var countMe = $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='undone']").text();
-                  if (countMe != response[i].manager_shipping_janky_count && sound == true) {
-                      //tutaj ding
-                      var snd = new Audio("{{asset('assets/1.mp3')}}");
-                      snd.play();
-                      sound = false;
-                      $('#check_messages_dkj').html('(!) <i class="fa fa-envelope fa-fw"></i><i class="fa fa-caret-down"></i>');
-                  }
-
-
-                  $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].shipping_all + ")");
-                  $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='count_yanek']").text(response[i].shipping_janky_count);
-                  $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='undone']").text(response[i].manager_shipping_janky_count);
-                  $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").removeClass("alert-danger");
-                  $("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").addClass("alert-success");
-
-                  i *= -1;
+									} else if (response[i].type == 'Badania') {
+											var countMe = $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text();
+											if (response[i].manager_research_janky_count != countMe && response[i].manager_research_janky_count > countMe) {
+													var sound = true;
+											}
+									} else if (response[i].type == 'Wysyłka') {
+											var countMe = $("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text();
+											if (response[i].manager_shipping_janky_count != countMe && response[i].manager_shipping_janky_count > countMe) {
+													var sound = true;
+											}
+									}
               }
+							//tutaj ding
+							if (sound == true) {
+									var snd = new Audio("{{asset('assets/1.mp3')}}");
+                  snd.play();
+                  sound = false;
+                  $('#check_messages_dkj').html('(!) <i class="fa fa-envelope fa-fw"></i><i class="fa fa-caret-down"></i>');
+
+									for (var i = 0; i < response.length; i++) {
+										if(response[i].type == 'Badania/Wysyłka') { // tutaj jezeli sa 2 rodzaje oddziału (badania/wysyłka)
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].research_all + ")");
+												$("#" + response[i].department_info_id + "dkjstatus td[name='count_yanek']").text(response[i].research_janky_count);
+												$("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text(response[i].manager_research_janky_count);
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").removeClass("alert-danger");
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").addClass("alert-success");
+
+												$("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].shipping_all + ")");
+												$("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='count_yanek']").text(response[i].shipping_janky_count);
+												$("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='undone']").text(response[i].manager_shipping_janky_count);
+												$("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").removeClass("alert-danger");
+												$("#" + response[i].department_info_id*(-1) + "dkjstatus td[name='status']").addClass("alert-success");
 
 
+										} else { // tutaj jezeli nie ma oddział nie jest podzielony na badania/wysyłkę
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").text("Odsłuchany (" + response[i].all_check_talk + ")");
+												$("#" + response[i].department_info_id + "dkjstatus td[name='count_yanek']").text(response[i].all_bad);
+												$("#" + response[i].department_info_id + "dkjstatus td[name='undone']").text(response[i].manager_research_janky_count);
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").removeClass("alert-danger");
+												$("#" + response[i].department_info_id + "dkjstatus td[name='status']").addClass("alert-success");
+										}
+									}
+							}
           }
       });
-    }, 20000);
+    }, 60000);
 @endif
 
 
