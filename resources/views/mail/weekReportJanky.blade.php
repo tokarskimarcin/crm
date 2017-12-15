8631fac4c2af45741ea5b72eee192d40cb6c27e4
konradja100 <konradja100@wp.pl>
2017-12-15 11:57:52 +0100

    Deleted some stuff

--- resources/views/mail/weekReportJanky.blade.php
+++ resources/views/mail/weekReportJanky.blade.php
@@ -38,27 +38,27 @@
         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$item->unchecked_janky}}</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$item->anulled_janky}}</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$item->deleted_janky}}</td>
-        @if($item->confirmed_janky != 0)
+        @if($item->confirmed_janky != 0 && $item->janky_sum)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round($item->confirmed_janky / $item->janky_sum * 100, 2)}} %</td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">0 %</td>
         @endif
-        @if($item->unconfirmed_janky != 0)
+        @if($item->unconfirmed_janky != 0 && $item->janky_sum)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round($item->unconfirmed_janky / $item->janky_sum * 100, 2)}} %</td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">0 %</td>
         @endif
-        @if($item->unchecked_janky != 0)
+        @if($item->unchecked_janky != 0 && $item->janky_sum)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round($item->unchecked_janky / $item->janky_sum * 100, 2)}} %</td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">0 %</td>
         @endif
-        @if($item->anulled_janky != 0)
+        @if($item->anulled_janky != 0 && $item->janky_sum)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round($item->anulled_janky / $item->janky_sum * 100, 2)}} %</td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">0 %</td>
         @endif
-        @if($item->deleted_janky != 0)
+        @if($item->deleted_janky != 0 && $item->janky_sum)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round($item->deleted_janky / $item->janky_sum * 100, 2)}} %</td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;">0 %</td>
@@ -82,27 +82,27 @@
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$total_unchecked_janky}}</b></td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$total_anulled_janky}}</b></td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$total_deleted_janky}}</b></td>
-        @if($total_confirmed_janky != 0)
+        @if($total_confirmed_janky != 0 && $total_janky_sum != 0)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round($total_confirmed_janky / $total_janky_sum * 100, 2)}} %</b></td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>0 %</b></td>
         @endif
-        @if($total_unconfirmed_janky != 0)
+        @if($total_unconfirmed_janky != 0 && $total_janky_sum != 0)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round($total_unconfirmed_janky / $total_janky_sum * 100, 2)}} %</b></td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>0 %</b></td>
         @endif
-        @if($total_unchecked_janky != 0)
+        @if($total_unchecked_janky != 0 && $total_janky_sum != 0)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round($total_unchecked_janky / $total_janky_sum * 100, 2)}} %</b></td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>0 %</b></td>
         @endif
-        @if($total_anulled_janky != 0)
+        @if($total_anulled_janky != 0 && $total_janky_sum != 0)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round($total_anulled_janky / $total_janky_sum * 100, 2)}} %</b></td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>0 %</b></td>
         @endif
-        @if($total_deleted_janky != 0)
+        @if($total_deleted_janky != 0 && $total_janky_sum != 0)
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round($total_deleted_janky / $total_janky_sum * 100, 2)}} %</b></td>
         @else
             <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>0 %</b></td>
