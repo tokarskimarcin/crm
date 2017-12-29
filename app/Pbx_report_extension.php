5a1bc9517b8edcda674b55c3f9a984833d972f3a
Sebastian Cytawa <spike123on@gmail.com>
2017-12-29 13:11:53 +0100

    .

---
+++ app/Pbx_report_extension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pbx_report_extension extends Model
+{
+    protected $table = 'pbx_report_extension';
+    public $timestamps = false;
+}
