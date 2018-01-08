3581679d3ab6157b8805dcadc6a82b4695a81338
konradja100 <konradja100@wp.pl>
2018-01-08 12:44:50 +0100

    Zamość test

--- app/Http/Middleware/CheckFirewall.php
+++ app/Http/Middleware/CheckFirewall.php
@@ -31,7 +31,7 @@ class CheckFirewall
         // 	   $acces = 1;
         // }
 
-        $dep = [1,2,6,7,10];
+        $dep = [1,2,6,7,10,11];
 
         if (in_array(Auth::user()->department_info_id, $dep)){
             if (Auth::user()->user_type_id != 3)
