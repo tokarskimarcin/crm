bc09496dbb0e2208abf5b82137de92bfaea0609e
konradja100 <konradja100@wp.pl>
2018-01-09 13:23:03 +0100

    Better view for show_nofication

--- resources/views/notifications/view_notification.blade.php
+++ resources/views/notifications/view_notification.blade.php
@@ -8,61 +8,96 @@
 </div>
 
 <div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-success">
+    <div class="col-md-4">
+        <div class="panel panel-info">
             <div class="panel-heading">
-                Opis problemu
+                <b>Tytuł</b>
             </div>
             <div class="panel-body">
-              <h3>
-                <p>
-                    <b>Tytuł:</b> {{$notification->title}}
-                </p>
-                <hr>
-                <p>
-                    <b>Typ problemu:</b> {{$notification->notification_type->name}}
-                </p>
-                <hr>
-                <p>
-                    <b>Oddział:</b> {{$notification->department_info->departments->name . ' ' . $notification->department_info->department_type->name}}
-                </p>
-                <hr>
-                <p>
-                    <b>Treść problemu:</b> {{$notification->content}}
-                </p>
-                <hr>
-                <p>
-                    <b>Status:</b>
-                    @if($notification->status == 1)
-                        Zgłoszono
-                    @elseif($notification->status == 2)
-                        Przyjęto do realizacji
-                    @elseif($notification->status == 3)
-                        Zakończono
-                    @endif
-                </p>
-                <hr>
-                @if(isset($notification->displayed_by) && $it_user != null)
-                    <p>
-                        <b>Pracownik IT:</b> {{$it_user->first_name . ' ' . $it_user->last_name}}
-                    </p>
-                    <hr>
-                @endif
-                <p>
-                  <b>Komentarze</b>
-                </p>
-                @foreach($notification->comments as $comment)
-                    <div class="well">
-                          {{$comment->content}}
-                          <p><small><b>Dodano:</b> {{$comment->created_at}} <b>przez</b> {{$comment->user->first_name . ' ' . $comment->user->last_name}}</small></p>
-                    </div>
-                @endforeach
-              </h3>
+                {{$notification->title}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <b>Typ problemu:</b>
+            </div>
+            <div class="panel-body">
+                {{$notification->notification_type->name}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <b>Oddział:</b>
+            </div>
+            <div class="panel-body">
+                {{$notification->department_info->departments->name . ' ' . $notification->department_info->department_type->name}}
             </div>
         </div>
     </div>
 </div>
 
+<div class="row">
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <b>Status:</b>
+            </div>
+            <div class="panel-body">
+              @if($notification->status == 1)
+                  Zgłoszono
+              @elseif($notification->status == 2)
+                  Przyjęto do realizacji
+              @elseif($notification->status == 3)
+                  Zakończono
+              @endif
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <b>Treść problemu:</b>
+            </div>
+            <div class="panel-body">
+                {{$notification->content}}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <b>Pracownik IT:</b>
+            </div>
+            <div class="panel-body">
+                @if(isset($notification->displayed_by) && $it_user != null)
+                    {{$it_user->first_name . ' ' . $it_user->last_name}}
+                @else
+                    Brak
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <h1>Komentarze:</h1>
+        @foreach($notification->comments as $comment)
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <b>Dodał: {{$comment->user->first_name . ' ' . $comment->user->last_name}} | {{$comment->created_at}}</b>
+                </div>
+                <div class="panel-body">
+                    {{$comment->content}}
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
 @section('script')
