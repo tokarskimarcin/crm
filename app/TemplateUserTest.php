dcf51aa4123de7ad98e84be3c9909748da3f85b4
konradja100 <konradja100@wp.pl>
2018-01-25 12:29:01 +0100

    Fukncie

--- app/TemplateUserTest.php
+++ app/TemplateUserTest.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class TemplateUserTest extends Model
 {
@@ -16,5 +17,22 @@ class TemplateUserTest extends Model
 
     public function templateQuestions() {
         return $this->belongsToMany('App\TestQuestion', 'template_questions', 'template_id', 'question_id');
+    }
+
+    public function questionsData() {
+        $query = DB::table('template_user_tests')
+            ->select(DB::raw('
+                template_user_tests.*,
+                template_questions.*,
+                test_questions.content as content,
+                test_categories.name as category_name
+            '))
+            ->join('template_questions', 'template_questions.template_id', 'template_user_tests.id')
+            ->join('test_questions', 'template_questions.question_id', 'test_questions.id')
+            ->join('test_categories', 'test_categories.id', 'test_questions.category_id')
+            ->where('template_user_tests.id', '=', $this->id)
+            ->get();
+
+        return $query;
     }
 }
--- app/UserTest.php
+++ app/UserTest.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class UserTest extends Model
 {
@@ -22,4 +23,19 @@ class UserTest extends Model
         return $this->belongsTo('App\User', 'checked_by');
     }
     
+    public function questionsData() {
+        $query = DB::table('user_tests')
+            ->select(DB::raw('
+                user_questions.*,
+                test_questions.content as content,
+                test_categories.name as category_name
+            '))
+            ->join('user_questions', 'user_questions.test_id', 'user_tests.id')
+            ->join('test_questions', 'test_questions.id', 'user_questions.question_id')
+            ->join('test_categories', 'test_categories.id', 'test_questions.category_id')
+            ->where('user_tests.id', '=', $this->id)
+            ->get();
+
+        return $query;
+    }
 }
