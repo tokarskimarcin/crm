9d38849d380aa5de1168027505ea6495e789e4b9
Sebastian Cytawa <spike123on@gmail.com>
2018-02-01 22:02:11 +0100

    Fix problem with phone number.

--- app/Http/Controllers/FinancesController.php
+++ app/Http/Controllers/FinancesController.php
@@ -38,6 +38,9 @@ class FinancesController extends Controller
             `users`.`agency_id`,
             `users`.`first_name`,
             `users`.`last_name`,
+            `users`.`username`,
+            `departments`.`name` as dep_name, 
+            `department_type`.`name`  as dep_type,
             `users`.`salary`,
             `users`.`additional_salary`,
             `users`.`student`,
@@ -45,6 +48,10 @@ class FinancesController extends Controller
             (SELECT SUM(`penalty_bonus`.`amount`) FROM `penalty_bonus` WHERE `penalty_bonus`.`id_user`=`users`.`id` AND `penalty_bonus`.`event_date` LIKE "'.$date.'" AND `penalty_bonus`.`type`=1 AND `penalty_bonus`.`status`=1) as `penalty`,
             (SELECT SUM(`penalty_bonus`.`amount`) FROM `penalty_bonus` WHERE `penalty_bonus`.`id_user`=`users`.`id` AND `penalty_bonus`.`event_date` LIKE  "'.$date.'" AND `penalty_bonus`.`type`=2 AND `penalty_bonus`.`status`=1) as `bonus`')
             ->where('users.status_work',1)
+            ->join('department_info','department_info.id','users.department_info_id')
+            ->join('departments','departments.id','department_info.id_dep')
+            ->join('department_type','department_type.id','department_info.id_dep_type')
+
             ->orderBy('users.last_name')->get();
         return view('finances.viewPaymentCadre')
             ->with('month',$date)
