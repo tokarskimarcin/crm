6ea33184a5b9e3c7a24d1030d75039ad0405df5a
Spik8 <spike123on@gmail.com>
2018-02-14 14:02:00 +0100

    Hide notifications

--- resources/views/partials/nav_includes/_bootstrap_notifications.blade.php
+++ resources/views/partials/nav_includes/_bootstrap_notifications.blade.php
@@ -1,24 +1,23 @@
 <script>
-    $(document).ready(function() {
-        $.notify({
-            title: 'Bootstrap notify',
-            message: 'Turning standard Bootstrap alerts into "notify" like notifications',
-            url: 'https://github.com/mouse0270/bootstrap-notify',
-            target: '_blank'
-        },{
-            // settings
-            type: 'warning',
-            delay: null,
-            animate: {
-                enter: 'animated lightSpeedIn',
-                exit: 'animated lightSpeedOut'
-            },
-            placement: {
-                from: "top",
-                align: "center"
-            },
-            offset: 50
-        });
-    
-    });
+//    $(document).ready(function() {
+//        $.notify({
+//            title: 'Bootstrap notify',
+//            message: 'Turning standard Bootstrap alerts into "notify" like notifications',
+//            url: 'https://github.com/mouse0270/bootstrap-notify',
+//            target: '_blank'
+//        },{
+//            // settings
+//            type: 'warning',
+//            delay: null,
+//            animate: {
+//                enter: 'animated lightSpeedIn',
+//                exit: 'animated lightSpeedOut'
+//            },
+//            placement: {
+//                from: "top",
+//                align: "center"
+//            },
+//            offset: 50
+//        });
+//    });
 </script>
