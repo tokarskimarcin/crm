bb9a73f88d67232e7ebaaa7bd689e9db38af4176
Spik8 <spike123on@gmail.com>
2018-03-20 14:39:23 +0100

    On mail

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -1511,7 +1511,7 @@ class StatisticsController extends Controller
             'data' => RecruitmentStory::getReportInterviewsData($date_start,$date_stop,0)
         ];
         $title = 'Dzienny Raport Rozmów Rekrutacyjnych '. date('Y-m-d');
-        $this->sendMailByVerona('recruitmentMail.dayReportInterviews', $data, $title);
+        $this->sendMailByVerona('recruitmentMail.dayReportHireCandidate', $data, $title);
     }
 
 
@@ -1538,7 +1538,7 @@ class StatisticsController extends Controller
             'data' => RecruitmentStory::getReportInterviewsData($date_start,$date_stop,0)
         ];
         $title = 'Tygodniowy Raport Rozmów Rekrutacyjnych '.$date_start.' - '.$date_stop;
-        $this->sendMailByVerona('recruitmentMail.weekReportInterviews', $data, $title);
+        $this->sendMailByVerona('recruitmentMail.weekReportHireCandidate', $data, $title);
     }
 
 
@@ -1569,7 +1569,7 @@ class StatisticsController extends Controller
             'data' => RecruitmentStory::getReportInterviewsData($date_start,$date_stop,0)
         ];
         $title = 'Miesięczny Raport Rozmów Rekrutacyjnych '.$date_start.' - '.$date_stop;
-        $this->sendMailByVerona('recruitmentMail.monthReportInterviews', $data, $title);
+        $this->sendMailByVerona('recruitmentMail.monthReportHireCandidate', $data, $title);
     }
 
 
@@ -2368,35 +2368,35 @@ return "Raport w przygotowaniu";
             $accepted_users->push($szczesny);
         }
 
-    $accepted_users = [
-        'cytawa.verona@gmail.com',
-        'jarzyna.verona@gmail.com'
-    ];
-
-        $mail_type = $mail_type_pom;
-     Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
-     {
-        $message->from('noreply.verona@gmail.com', 'Verona Consulting');
-        foreach ($accepted_users as $key => $user) {
-          if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
-              $message->to($user)->subject($mail_title);
-          }
-        }
-     });
+//    $accepted_users = [
+//        'cytawa.verona@gmail.com',
+//        'jarzyna.verona@gmail.com'
+//    ];
 
 //        $mail_type = $mail_type_pom;
-//      /* UWAGA !!! ODKOMENTOWANIE TEGO POWINNO ZACZĄC WYSYŁAĆ MAILE*/
-//       Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
-//       {
-//           $message->from('noreply.verona@gmail.com', 'Verona Consulting');
-//           foreach($accepted_users as $user) {
-//            if (filter_var($user->username, FILTER_VALIDATE_EMAIL)) {
-//                $message->to($user->username, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
-//             }
-//             if (filter_var($user->email_off, FILTER_VALIDATE_EMAIL)) {
-//                $message->to($user->email_off, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
-//             }
-//           }
-//       });
+//     Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
+//     {
+//        $message->from('noreply.verona@gmail.com', 'Verona Consulting');
+//        foreach ($accepted_users as $key => $user) {
+//          if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
+//              $message->to($user)->subject($mail_title);
+//          }
+//        }
+//     });
+
+        $mail_type = $mail_type_pom;
+      /* UWAGA !!! ODKOMENTOWANIE TEGO POWINNO ZACZĄC WYSYŁAĆ MAILE*/
+       Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
+       {
+           $message->from('noreply.verona@gmail.com', 'Verona Consulting');
+           foreach($accepted_users as $user) {
+            if (filter_var($user->username, FILTER_VALIDATE_EMAIL)) {
+                $message->to($user->username, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
+             }
+             if (filter_var($user->email_off, FILTER_VALIDATE_EMAIL)) {
+                $message->to($user->email_off, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
+             }
+           }
+       });
     }
 }
