cbee4024e1d5a13d9ff48750aa9072a6fafe1990
Spik8 <spike123on@gmail.com>
2018-03-20 11:00:09 +0100

    On mail

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2083,35 +2083,35 @@ class StatisticsController extends Controller
 
 
 
-    $accepted_users = [
-        'cytawa.verona@gmail.com',
-        'jarzyna.verona@gmail.com'
-    ];
-
-        $mail_type = $mail_type_pom;
-     Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
-     {
-        $message->from('noreply.verona@gmail.com', 'Verona Consulting');
-        foreach ($accepted_users as $key => $user) {
-          if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
-              $message->to($user)->subject($mail_title);
-          }
-        }
-     });
+//    $accepted_users = [
+//        'cytawa.verona@gmail.com',
+//        'jarzyna.verona@gmail.com'
+//    ];
+//
+//        $mail_type = $mail_type_pom;
+//     Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
+//     {
+//        $message->from('noreply.verona@gmail.com', 'Verona Consulting');
+//        foreach ($accepted_users as $key => $user) {
+//          if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
+//              $message->to($user)->subject($mail_title);
+//          }
+//        }
+//     });
 //
         $mail_type = $mail_type_pom;
       /* UWAGA !!! ODKOMENTOWANIE TEGO POWINNO ZACZĄC WYSYŁAĆ MAILE*/
-//       Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
-//       {
-//           $message->from('noreply.verona@gmail.com', 'Verona Consulting');
-//           foreach($accepted_users as $user) {
-//            if (filter_var($user->username, FILTER_VALIDATE_EMAIL)) {
-//                $message->to($user->username, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
-//             }
-//             if (filter_var($user->email_off, FILTER_VALIDATE_EMAIL)) {
-//                $message->to($user->email_off, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
-//             }
-//           }
-//       });
+       Mail::send('mail.' . $mail_type, $data, function($message) use ($accepted_users, $mail_title)
+       {
+           $message->from('noreply.verona@gmail.com', 'Verona Consulting');
+           foreach($accepted_users as $user) {
+            if (filter_var($user->username, FILTER_VALIDATE_EMAIL)) {
+                $message->to($user->username, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
+             }
+             if (filter_var($user->email_off, FILTER_VALIDATE_EMAIL)) {
+                $message->to($user->email_off, $user->first_name . ' ' . $user->last_name)->subject($mail_title);
+             }
+           }
+       });
     }
 }
