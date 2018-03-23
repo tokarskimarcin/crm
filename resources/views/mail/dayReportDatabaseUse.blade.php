7341c0b9f74b32190a9b40553193a65bddc14e2b
Spik8 <spike123on@gmail.com>
2018-03-23 08:47:33 +0100

    Fix database use statistics

--- resources/views/mail/dayReportDatabaseUse.blade.php
+++ resources/views/mail/dayReportDatabaseUse.blade.php
@@ -1,9 +1,9 @@
 <table style ="width:100%;border:1px solid #231f20;border-collapse:collapse;padding:3px;">
 <thead style="color:#efd88f;">
 <tr>
-<td colspan="5" style="border:1px solid #231f20;text-align:center;padding:3px;background:#231f20; color:#efd88f;">
+<td colspan="6" style="border:1px solid #231f20;text-align:center;padding:3px;background:#231f20; color:#efd88f;">
     <font size="5" face="sans-serif">Dzienny Raport Wykorzystania Bazy {{date("Y-m-d",mktime(0,0,0,date("m"),date("d")-1,date("Y")))}}</font></td>
-<td colspan="5" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20;">
+<td colspan="6" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20;">
 <img src="http://teambox.pl/image/logovc.png"></td>
 </tr>
 <tr>
@@ -11,10 +11,13 @@
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Imie</th>
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Nazwisko</th>
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Bisnode</th>
-    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Zgody</th>
+    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Bisnode Zgody</th>
+    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Stare Zgody</th>
+    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Nowe Zgody</th>
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Event</th>
+    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Event Zgody</th>
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Reszta</th>
-    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Exito</th>
+    <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Reszta Zgody</th>
     <th style="border:1px solid #231f20;padding:3px;background:#231f20;">Suma</th>
 </tr>
 </thead>
@@ -26,10 +29,13 @@
     <tr>
         <td colspan=3 style="border:1px solid #231f20;text-align:center;padding:3px;"><b>Ogół</b></td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->bisnode*100)/$overall_result[0]->suma)}} %</td>
+        <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->bisnodeZgody*100)/$overall_result[0]->suma)}} %</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->zgody*100)/$overall_result[0]->suma)}} %</td>
+        <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->zgodyZgody*100)/$overall_result[0]->suma)}} %</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->event*100)/$overall_result[0]->suma)}} %</td>
+        <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->eventZgody*100)/$overall_result[0]->suma)}} %</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->reszta*100)/$overall_result[0]->suma)}} %</td>
-        <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->exito*100)/$overall_result[0]->suma)}} %</td>
+        <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($overall_result[0]->resztaZgody*100)/$overall_result[0]->suma)}} %</td>
         <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$overall_result[0]->suma}}</td>
     </tr>
         @foreach($departments_statistic as $item)
@@ -37,10 +43,13 @@
             <tr>
                 <td colspan=3 style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$item->name}}</b></td>
                 <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->bisnode*100)/$item->suma)}} %</b></td>
+                <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->bisnodeZgody*100)/$item->suma)}} %</b></td>
                 <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->zgody*100)/$item->suma)}} %</b></td>
+                <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->zgodyZgody*100)/$item->suma)}} %</b></td>
                 <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->event*100)/$item->suma)}} %</b></td>
+                <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->eventZgody*100)/$item->suma)}} %</b></td>
                 <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->reszta*100)/$item->suma)}} %</b></td>
-                <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->exito*100)/$item->suma)}} %</b></td>
+                <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{round(($item->resztaZgody*100)/$item->suma)}} %</b></td>
                 <td style="border:1px solid #231f20;text-align:center;padding:3px;"><b>{{$item->suma}}</b></td>
             </tr>
             @foreach($employee_statistic as $value)
@@ -50,10 +59,13 @@
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$value->name}}</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$value->last}}</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->bisnode*100)/$value->suma)}} %</td>
+                        <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->bisnodeZgody*100)/$value->suma)}} %</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->zgody*100)/$value->suma)}} %</td>
+                        <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->zgodyZgody*100)/$value->suma)}} %</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->event*100)/$value->suma)}} %</td>
+                        <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->eventZgody*100)/$value->suma)}} %</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->reszta*100)/$value->suma)}} %</td>
-                        <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->exito*100)/$value->suma)}} %</td>
+                        <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{round(($value->resztaZgody*100)/$value->suma)}} %</td>
                         <td style="border:1px solid #231f20;text-align:center;padding:3px;">{{$value->suma}}</td>
                     </tr>
                 @endif
