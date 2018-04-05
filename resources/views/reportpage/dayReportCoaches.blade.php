0b0b9246eb9dd1a1d5e380b25b011b0dba961887
konradja100 <konradja100@wp.pl>
2018-04-05 09:09:29 +0200

    Add new breadcrumbs

--- resources/views/reportpage/dayReportCoaches.blade.php
+++ resources/views/reportpage/dayReportCoaches.blade.php
@@ -4,7 +4,9 @@
     {{--Header page --}}
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Raport Dzienny Trenerzy</h1>
+            <div class="page-header">
+                <div class="alert gray-nav">Raport Dzienny Trenerzy</div>
+            </div>
         </div>
     </div>
     <form method="POST" action="{{ URL::to('/pageDayReportCoaches') }}" id="my_coach_form">
