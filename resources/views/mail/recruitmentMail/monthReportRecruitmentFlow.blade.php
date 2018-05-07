f7b83e3a7a0efc131be7a5e04073c2fe6e215e4d
pawelverona <salata02@gmail.com>
2018-05-07 14:06:49 +0200

    [RECRUITMENT FLOW] Fixed banner bug

--- resources/views/mail/recruitmentMail/monthReportRecruitmentFlow.blade.php
+++ resources/views/mail/recruitmentMail/monthReportRecruitmentFlow.blade.php
@@ -1,13 +1,19 @@
 @php
     $id = 0;
+    $number_of_sources = 0;
 @endphp
+@foreach($data['source'] as $src)
+    @php
+        $number_of_sources++;
+    @endphp
+@endforeach
 
 <table style="width:100%;border:1px solid #231f20;border-collapse:collapse;padding:3px">
     <thead style="color:#efd88f">
     <tr>
         <td colspan="1" style="border:1px solid #231f20;text-align:center;padding:3px;background:#231f20;color:#efd88f">
             <font size="6" face="Calibri">Miesięczne Statystyki Spływu Rekrutacji  </font></td>
-        <td colspan="5" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20">
+        <td colspan="{{$number_of_sources + 1}}" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20">
             <img src="http://teambox.pl/image/logovc.png" class="CToWUd"></td>
     </tr>
     <tr>
