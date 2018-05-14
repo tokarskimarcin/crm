ce42c2b029c81a0c38504b112f7b8f57c71ce430
spik8 <skobry123on@gmail.com>
2018-05-14 09:15:06 +0200

    set mail report ranking - one day (last week 1 - 13)

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4188,13 +4188,14 @@ public function getCoachingDataAllLevel($month, $year, $dep_id,$level_coaching){
     }
 
     public function weekReportDepartmentsRanking(){
-        $toDay = date('Y-m-d');
+       // $toDay = date('Y-m-d');
+        $toDay = date("Y-m-d",mktime(0,0,0,date("m"),date("d")-1,date("Y")));
         $data = $this::setDataReportDepartmentsRanking($toDay);
-//        $user = User::where('id','=',1364)->get();
-        if($data['weekLastDay'] == $toDay){
+        $user = User::where('id','=',1364)->get();
+       // if($data['weekLastDay'] == $toDay){
             $title = 'Raport Tygodniowy Filii '.$data['week'].' - '.$data['weekLastDay'];
-            $this->sendMailByVerona('weekReportDepartmentsRanking', $data, $title);
-        }
+            $this->sendMailByVerona('weekReportDepartmentsRanking', $data, $title,$user);
+        //}
     }
 
     /*
