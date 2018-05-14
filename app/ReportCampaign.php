4fdf938b7e457a2684655fe92407764e62aeee99
pawelverona <salata02@gmail.com>
2018-05-14 09:33:06 +0200

    [MODEL] Added report campaign model

---
+++ app/ReportCampaign.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReportCampaign extends Model
+{
+    protected $table = 'report_campaign';
+    public $timestamps = false;
+}
