21525c763bca1ee5627e25ccf364b6ede3fb362a
pawelverona <salata02@gmail.com>
2018-05-15 15:01:30 +0200

    [CRM] TEMPLATE

--- resources/views/crmRoute/index.blade.php
+++ resources/views/crmRoute/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.main')
 @section('style')
-    <style>
-        .panel-body {
-            background-color: #ebecf1;
-        }
-    </style>
+
 @endsection
 @section('content')
 
@@ -47,6 +43,153 @@
 
 
     $(document).ready(function() {
+
+        function Stack() {
+            let items = [];
+            this.push = function(element) { //add element to stack
+                items.push(element);
+            };
+            this.pop = function() { //remove element from stack
+                return items.pop();
+            };
+            this.peek = function() { //what is top element
+                return items[items.length - 1];
+            };
+            this.isEmpty = function() { //sprawdza czy jest pusta true = tak/ false = nie
+                return items.length == 0;
+            };
+            this.size = function() {  // return lenght of queue
+                return items.length;
+            };
+            this.print = function() { //print queue elements
+                console.log(items.toString());
+            };
+            this.clear = function() {
+                items = [];
+            }
+        }
+
+        /**
+         *Ta funkcja tworzy nowy route
+         */
+        function create_new_route() {
+            let number_of_route = route_stack.size() + 1;
+
+            newElement = document.createElement('div');
+            newElement.className = 'routes-container';
+            newElement.innerHTML = '        <div class="row">\n' +
+                    '<div class="button_section button_section_gl_nr' + number_of_route + '">' +
+                    '<span class="glyphicon glyphicon-remove" id="remove-route"></span>' +
+                    '</div>' +
+                '        <header>Trasa #' + number_of_route + '</header>\n' +
+                '\n' +
+                '            <div class="col-md-4">\n' +
+                '                <div class="form-group">\n' +
+                '                    <label for="woj' + number_of_route + '">Województwo</label>\n' +
+                '                    <select name="woj' + number_of_route + '" id="woj' + number_of_route + '" class="form-control">\n' +
+                '                        <option value="0">Wybierz</option>\n' +
+                '                        <option value="1">Lubelskie</option>\n' +
+                '                        <option value="2">Mazowieckie</option>\n' +
+                '                    </select>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="col-md-4">\n' +
+                '                <div class="form-group">\n' +
+                '                    <label for="city' + number_of_route + '">Miasto</label>\n' +
+                '                    <select name="city' + number_of_route + '" id="city' + number_of_route + '" class="form-control">\n' +
+                '                        <option value="0">Wybierz</option>\n' +
+                '                        <option value="1">Lublin</option>\n' +
+                '                        <option value="2">Świdnik</option>\n' +
+                '                    </select>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="col-md-4">\n' +
+                '                <div class="form-group">\n' +
+                '                    <label for="karencja' + number_of_route + '">Karencja</label>\n' +
+                '                    <select name="karencja' + number_of_route + '" id="karencja' + number_of_route + '" class="form-control">\n' +
+                '                        <option>Wybierz</option>\n' +
+                '                        <option value="0">0</option>\n' +
+                '                        <option value="1">1</option>\n' +
+                '                        <option value="2">2</option>\n' +
+                '                        <option value="3">3</option>\n' +
+                '                    </select>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="form-group">\n' +
+                '                <label for="date">Data:</label>\n' +
+                '                <div class="input-group date form_date col-md-5" data-date="" data-date-format="yyyy-mm-dd" data-link-field="datak" style="width:100%;">\n' +
+                '                    <input class="form-control" name="start_date' + number_of_route + '" type="text">\n' +
+                '                    <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="col-lg-12 button_section button_section_nr' + number_of_route + '">\n' +
+                '                \n' +
+                '                <input type="button" class="btn btn-success" id="add_new_routes" value="Zapisz!" style="width:100%;">\n' +
+                '<input type="button" class="btn btn-info btn_add_new_route" id="add_new_route" value="Dodaj nową trasę" style="width:100%;margin-top:1em;margin-bottom:1em;">' +
+                '            </div>\n' +
+                '        </div>';
+            return newElement;
+        }
+
+        /**
+         * Ta funkcja usuwa wszystkie buttony z dotychczasowych routów
+         */
+        function clear_button_section() {
+            let button_section = Array.from(document.getElementsByClassName('button_section'));
+            button_section.forEach(function(section) {
+                section.textContent = '';
+            });
+        }
+
+        /**
+         * Ta funkcja dodaje nowy route
+         */
+        function add_new_route() {
+            let new_route = create_new_route(); //otrzymujemy nowy formularz
+            clear_button_section();
+            route_stack.push(new_route); //dodajemy go do stosu
+            let main_container = document.querySelector('.routes-wrapper'); //zaznaczamy główny container
+            main_container.appendChild(new_route); //dodajemy nowy element jako ostatnie dziecko
+
+            $('.form_date').datetimepicker({
+                language:  'pl',
+                autoclose: 1,
+                minView : 2,
+                pickTime: false
+            });
+        }
+
+        function click_button_handler(e) {
+            if(e.target.id == 'add_new_route') {
+                add_new_route();
+            }
+            if(e.target.id == "remove-route") {
+                let last_route = document.getElementsByClassName('routes-container')[route_stack.size() - 1];
+                let route_wrapper = document.getElementsByClassName('routes-wrapper')[0];
+                route_wrapper.removeChild(last_route); //deleta last route
+
+                let button_div_class_name = 'button_section_nr' + (route_stack.size() - 1);
+                let button_div_gl_class_name = 'button_section_gl_nr' + (route_stack.size() - 1);
+                let pre_last_button_div = document.getElementsByClassName(button_div_class_name)[0]; //select previous route's buttons div
+                let pre_last_button_gl_div = document.getElementsByClassName(button_div_gl_class_name)[0]; //select previous route's buttons div
+                pre_last_button_div.innerHTML = '                <input type="button" class="btn btn-success" id="add_new_routes" value="Zapisz!" style="width:100%;">\n' +
+                '<input type="button" class="btn btn-info btn_add_new_route" id="add_new_route" value="Dodaj nową trasę" style="width:100%;margin-top:1em;margin-bottom:1em;">';
+                pre_last_button_gl_div.innerHTML = '<span class="glyphicon glyphicon-remove" id="remove-route"></span>';
+
+                route_stack.pop();
+            }
+        }
+
+        let route_stack = new Stack();
+
+        add_new_route();
+
+        let routes_wrapper = document.getElementsByClassName('routes-wrapper')[0];
+        routes_wrapper.addEventListener('click', click_button_handler);
 
     });
 
--- resources/views/crmRoute/routes.blade.php
+++ resources/views/crmRoute/routes.blade.php
@@ -8,15 +8,19 @@
     }
 
     .routes-container {
-        border: 1px solid lightgray;
         background-color: white;
         padding: 2em;
+        box-shadow: 0 1px 15px 1px rgba(39,39,39,.1);
+        border: 0;
+        border-radius: .1875rem;
+        margin: 1em;
 
         display: flex;
         flex-direction: column;
         justify-content: center;
         min-width: 90%;
         max-width: 90%;
+
     }
 
     header {
@@ -25,61 +29,20 @@
         font-weight: bold;
     }
 
-
-
+    .glyphicon-remove {
+        font-size: 2em;
+        transition: all 0.8s ease-in-out;
+        float: right;
+        color:red;
+    }
+    .glyphicon-remove:hover {
+        transform: scale(1.2) rotate(180deg);
+        cursor: pointer;
+    }
 
 </style>
 
-
-<div class="row">
-    <div class="col-lg-12">
-        <input type="button" class="btn btn-info" id="add_new_route" value="Dodaj nową trasę" style="width:100%;margin:1em;">
-    </div>
-</div>
 <div class="routes-wrapper">
-    <div class="routes-container">
 
-        <header>Trasa #1</header>
-
-        <section>
-            <div class="form-group">
-                <label for="woj">Województwo</label>
-                <select name="" id="woj" class="form-control">
-                    <option value="0">Wybierz</option>
-                    <option value="1">Lubelskie</option>
-                    <option value="2">Mazowieckie</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="city">Miasto</label>
-                <select name="" id="city" class="form-control">
-                    <option value="0">Wybierz</option>
-                    <option value="1">Lublin</option>
-                    <option value="2">Świdnik</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="karencja">Karencja</label>
-                <select name="" id="karencja" class="form-control">
-                    <option>Wybierz</option>
-                    <option value="0">0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="date">Data:</label>
-                <div class="input-group date form_date col-md-5" data-date="" data-date-format="yyyy-mm-dd" data-link-field="datak" style="width:100%;">
-                    <input class="form-control" name="start_date" type="text">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
-                </div>
-            </div>
-
-        </section>
-    </div>
 </div>
 
