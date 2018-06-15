fea76111f495f2bb50b7499cd039af893c2933d6
pawelverona <salata02@gmail.com>
2018-06-15 10:23:29 +0200

    [CRM] Added table template

--- resources/views/crmRoute/showRoutesDetailed.blade.php
+++ resources/views/crmRoute/showRoutesDetailed.blade.php
@@ -22,7 +22,7 @@
 
                 </div>
                 <div class="panel-body">
-                        <table id="datatable">
+                        <table id="datatable" class="thead-inverse table table-striped table-bordered">
                             <thead>
                             <tr>
                                 <th>Tydzien</th>
@@ -33,9 +33,120 @@
                                 <th>Limit</th>
                                 <th>Straty</th>
                                 <th>Projekt</th>
-                                <th>Uwagi</th>
+                                <th>Departament</th>
+                                <td>Uwagi</td>
                             </tr>
                             </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>01.02.2018</td>
+                                <td>Lublin</td>
+                                <td>50</td>
+                                <td>60</td>
+                                <td>70</td>
+                                <td>-10</td>
+                                <td>Damages</td>
+                                <td>Lublin Telemarketing</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>01.02.2018</td>
+                                <td>Lublin</td>
+                                <td>40</td>
+                                <td>65</td>
+                                <td>70</td>
+                                <td>-5</td>
+                                <td>Damages</td>
+                                <td>Lublin Telemarketing</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>01.02.2018</td>
+                                <td>Lublin</td>
+                                <td>60</td>
+                                <td>70</td>
+                                <td>70</td>
+                                <td>0</td>
+                                <td>Exito</td>
+                                <td>Radom Potwierdzenia</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>01.02.2018</td>
+                                <td>Lublin</td>
+                                <td>45</td>
+                                <td>70</td>
+                                <td>70</td>
+                                <td>0</td>
+                                <td>Exito</td>
+                                <td>Radom Potwierdzenia</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr style="font-weight: bold; background: lightslategrey; font-size:1.1em;">
+                                <td>1</td>
+                                <td>01.02.2018</td>
+                                <td></td>
+                                <td></td>
+                                <td>335</td>
+                                <td></td>
+                                <td>-15</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>02.02.2018</td>
+                                <td>Warszawa/mokotow</td>
+                                <td>10</td>
+                                <td>35</td>
+                                <td>40</td>
+                                <td>-5</td>
+                                <td>Vitautas</td>
+                                <td>Białystok Telemarketing</td>
+                                <td>Słabo</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>02.02.2018</td>
+                                <td>Warszawa/mokotow</td>
+                                <td>15</td>
+                                <td>35</td>
+                                <td>40</td>
+                                <td>-5</td>
+                                <td>Vitautas</td>
+                                <td>Białystok Telemarketing</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>02.02.2018</td>
+                                <td>Warszawa/mokotow</td>
+                                <td>20</td>
+                                <td>35</td>
+                                <td>35</td>
+                                <td>0</td>
+                                <td>Vitautas</td>
+                                <td>Ostrowiec Telemarketing</td>
+                                <td>Brak</td>
+                            </tr>
+                            <tr style="font-weight: bold; background: lightslategrey; font-size:1.1em;">
+                                <td>1</td>
+                                <td>02.02.2018</td>
+                                <td></td>
+                                <td></td>
+                                <td>105</td>
+                                <td></td>
+                                <td>-10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
                         </table>
                     <div class="row">
                     </div>
@@ -47,72 +158,75 @@
 @endsection
 
 @section('script')
-   <script>
+    <script src="{{ asset('/js/dataTables.bootstrap.min.js')}}"></script>
+    <script>
        document.addEventListener('DOMContentLoaded', function(mainEvent) {
 
-           table = $('#datatable').DataTable({
-               "autoWidth": true,
-               "processing": true,
-               "serverSide": true,
-               "drawCallback": function( settings ) {
+           table = $('#datatable').DataTable();
 
-               },
-               "rowCallback": function( row, data, index ) {
-                   // $(row).attr('id', "client_" + data.id);
-                   // return row;
-               },"fnDrawCallback": function(settings) {
+           {{--table = $('#datatable').DataTable({--}}
+               {{--"autoWidth": true,--}}
+               {{--"processing": true,--}}
+               {{--"serverSide": true,--}}
+               {{--"drawCallback": function( settings ) {--}}
+
+               {{--},--}}
+               {{--"rowCallback": function( row, data, index ) {--}}
+                   {{--// $(row).attr('id', "client_" + data.id);--}}
+                   {{--// return row;--}}
+               {{--},"fnDrawCallback": function(settings) {--}}
 
 
-               },"ajax": {
-                   'url': "{{route('api.getDetailedInfo')}}",
-                   'type': 'POST',
-                   'data': function (d) {
+               {{--},"ajax": {--}}
+                   {{--'url': "{{route('api.getDetailedInfo')}}",--}}
+                   {{--'type': 'POST',--}}
+                   {{--'data': function (d) {--}}
 
-                   },
-                   'headers': {'X-CSRF-TOKEN': '{{ csrf_token() }}'}
-               },
-               "language": {
-                   "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Polish.json"
-               },
-               "columns":[
-                   {"data":function (data, type, dataToSet) {
-                           return data.weekOfYear;
-                       },"name":"weekOfYear"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return data.date;
-                       },"name":"date"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name3"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name4"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name5"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name6"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name7"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name8"
-                   },
-                   {"data":function (data, type, dataToSet) {
-                           return 1;
-                       },"name":"name9"
-                   }
-               ]
-           });
+                   {{--},--}}
+                   {{--'headers': {'X-CSRF-TOKEN': '{{ csrf_token() }}'}--}}
+               {{--},--}}
+               {{--"language": {--}}
+                   {{--"url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Polish.json"--}}
+               {{--},--}}
+               {{--"columns":[--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return data.weekOfYear;--}}
+                       {{--},"name":"weekOfYear"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return data.date;--}}
+                       {{--},"name":"date"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name3"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name4"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name5"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name6"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name7"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name8"--}}
+                   {{--},--}}
+                   {{--{"data":function (data, type, dataToSet) {--}}
+                           {{--return 1;--}}
+                       {{--},"name":"name9"--}}
+                   {{--}--}}
+               {{--]--}}
+           {{--});--}}
        });
     </script>
 @endsection
