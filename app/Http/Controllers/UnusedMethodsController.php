5b7fdd1e46edd6d34d488450b98b81249e2ad3c9
tokarskimarcin <marcinxx1994@gmail.com>
2018-06-28 14:02:47 +0200

    Unused methods from StatisticController moved to UnusedMethodsController

---
+++ app/Http/Controllers/UnusedMethodsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UnusedMethodsController extends Controller
+{
+    // wyswietlenie raportu godzinnego do uzupełnienia
+    public function hourReportGet()
+    {
+        $today = date('Y-m-d');
+        $reports = HourReport::where('report_date','like',$today)
+            ->where('department_info_id',Auth::user()->department_info_id)->get();
+        return view('statistics.hourReport')
+            ->with('reports',$reports);
+    }
+    // wyswietlenie raportu godzinnego do uzupełnienia i zapisu
+    public function hourReportPost(Request $request)
+    {
+        $today = date('Y-m-d');
+        $hour = $request->hour;
+        $average = $request->average;
+        $success = $request->success;
+        $employee_count = $request->employee_count;
+        $janky_count = $request->janky_count;
+        $wear_base = $request->wear_base;
+        $call_Time = $request->call_Time;
+        $find_report = HourReport::where('hour',$hour)
+            ->where('report_date',$today)
+            ->where('department_info_id',Auth::user()->department_info_id)
+            ->first();
+        if($find_report == null)
+        {
+            $newRaport = new HourReport();
+            $newRaport->user_id = Auth::user()->id;
+            $newRaport->department_info_id = Auth::user()->department_info_id;
+            $newRaport->report_date = $today;
+            $newRaport->hour = $hour;
+            $newRaport->average = $average;
+            $newRaport->success = $success;
+            $newRaport->employee_count = $employee_count;
+            $newRaport->janky_count = $janky_count;
+            $newRaport->wear_base = $wear_base;
+            $newRaport->call_Time = $call_Time;
+            $newRaport->save();
+            $message = "Raport został dodany.";
+            $status = 1;
+        }else
+        {
+            $message = "Raport nie został dodany, ponieważ jest już wysłany.";
+            $status = 0;
+        }
+        return redirect()->back()
+            ->with('message',$message)
+            ->with('status',$status);
+    }
+    // edycja wpisywanego raportu godzinnego
+    public function hourReportEditPost(Request $request)
+    {
+        $today = date('Y-m-d');
+        $record_id = $request->record_id;
+        $average = $request->average;
+        $success = $request->success;
+        $employee_count = $request->employee_count;
+        $janky_count = $request->janky_count;
+        $wear_base = $request->wear_base;
+        $call_Time = $request->call_Time;
+        $newRaport = HourReport::find($record_id);
+        if ($newRaport != null) {
+            if($newRaport->is_send == 0)
+            {
+                $newRaport->user_id = Auth::user()->id;
+                $newRaport->department_info_id = Auth::user()->department_info_id;
+                $newRaport->average = $average;
+                $newRaport->success = $success;
+                $newRaport->employee_count = $employee_count;
+                $newRaport->janky_count = $janky_count;
+                $newRaport->wear_base = $wear_base;
+                $newRaport->call_Time = $call_Time;
+                $newRaport->save();
+                $message = "Raport został zmieniony";
+                $status = 1;
+            }else
+            {
+                $message = "Raport nie został zmienione, ponieważ jest już wysłany.";
+                $status = 0;
+            }
+            return redirect()->back()
+                ->with('message',$message)
+                ->with('status',$status);
+        } else {
+            return view('errors.404');
+        }
+    }
+
+
+}
