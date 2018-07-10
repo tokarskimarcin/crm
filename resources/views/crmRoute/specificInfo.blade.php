e0112912f8c531cf8767c4a200accf7e8845ebcd
spik8 <sedes123>
2018-07-10 12:12:28 +0200

    add pirce for hotel and user who will made it

--- resources/views/crmRoute/specificInfo.blade.php
+++ resources/views/crmRoute/specificInfo.blade.php
@@ -80,6 +80,13 @@
                 </div>
             </div>
         </div>
+        <div class="client-info-container" style="width: 49%">
+            <div class="row">
+                <div class="col-lg-6" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
+                    <label for="user_reservation" >Osoba Rezerwująca : </label>
+                    <input id="user_reservation" name="user_reservation" type="text" class="form-control price-input" @if(isset($user_reservation)) value="{{$user_reservation}}" @else value="Brak"@endif>
+                </div>
+        </div>
     </div>
 
     <div class="client-wrapper">
@@ -123,6 +130,7 @@
                                 <th>Nazwa</th>
                                 <th>Wojewodztwo</th>
                                 <th>Miasto</th>
+                                <th>Ulica</th>
                                 <th>Kod Pocztowy</th>
                                 <th>Wybierz</th>
                             </tr>
@@ -134,6 +142,10 @@
                             <label>Godzina pokazu nr. @php echo $i; @endphp</label>
                             <input type="time" class="form-control time-input" @if(isset($item->hour)) value="{{$item->hour}}" @endif>
                         </div>
+                            <div class="form-group">
+                                <label>Cena za salę</label>
+                                <input id="hotel_price" type="number" class="form-control price-input" @if(isset($item->hotel_price)) value="{{$item->hotel_price}}" @endif>
+                            </div>
                         @if(!$loop->last)
                             <div style="width: 100%; height:2px; border-top:3px dashed black; margin-top:1em; margin-bottom:1em;"></div>
                         @endif
@@ -278,6 +290,10 @@
                                 return data.cityName;
                             },"name":"cityName", "orderable": false
                         },
+                            {"data": function(data, type, dataToSet) {
+                                    return data.street;
+                                },"name":"street", "orderable": false
+                            },
                         {"data": function(data, type, dataToSet) {
                                /* zipCode = String(data.zip_code);
                                 if(zipCode != 'null') {
@@ -328,16 +344,20 @@
             });*/
 
             //This object will store every info about given city.
-            function CityObject(voivode_id, city_id, time_hotel_arr, client_route_id) {
+            function CityObject(voivode_id, city_id, time_hotel_arr, client_route_id,price_hotel_arr) {
                 this.voivodeId = voivode_id;
                 this.cityId = city_id;
                 this.clientRouteId = client_route_id;
                 this.timeHotelArr = time_hotel_arr;
+                this.priceHotelArr = price_hotel_arr;
+                this.user_reservation = document.getElementById('user_reservation').value;
                 this.showValues = function() {
                     console.log("voivodeId: " + voivode_id);
                     console.log("cityId: " + city_id);
                     console.log("timeArr: " + time_arr);
+                    console.log("priceArr: " + price_hotel_arr);
                     console.log("clientRouteId: " + client_route_id);
+                    console.log("user_reservation: " + user_reservation);
                     console.log("hotelId: " + hotel_id);
                 };
                 this.validate = function() {
@@ -370,7 +390,9 @@
                         voivodeId: this.voivodeId,
                         cityId: this.cityId,
                         timeHotelArr: this.timeHotelArr,
+                        priceHotelArr: this.priceHotelArr,
                         clientRouteId: this.clientRouteId,
+                        user_reservation: this.user_reservation
                     };
                     cityInfoArray.push(obj);
 
@@ -394,6 +416,24 @@
 
             }
 
+            function createPriceHotelArrayOfObjects(cityContainer) {
+                const allHotels = cityContainer.querySelectorAll('table');
+                const allPrice = cityContainer.querySelectorAll('.price-input');
+                let priceHotelArr = [];
+                for(let i = 0; i < allHotels.length; i++) { //number of hotels = number of times
+                    const hotelId = allHotels[i].querySelector('input[type="checkbox"]:checked') == null ? null : allHotels[i].querySelector('input[type="checkbox"]:checked').value; //null or id
+                    const price = allPrice[i].value;
+                    const priceHotelObject = {
+                        hotelId: hotelId,
+                        price: price
+                    };
+                    priceHotelArr.push(priceHotelObject);
+                }
+                return priceHotelArr;
+            }
+
+
+
             //this function collect data from forms and validate it.
             function submitHandler(e) {
                 let isOk = null;
@@ -405,11 +445,11 @@
                     const cityElement = cityContainer.querySelector('.city_info'); //city element
                     const clientRouteId = cityContainer.querySelector('.idOfClientRoute').value;
                     const timeHotelArr = createTimeHotelArrayOfObjects(cityContainer);
-
+                    const priceHotelArr = createPriceHotelArrayOfObjects(cityContainer);
                     const voivodeId = voivodeElement.dataset.identificator;
                     const cityId = cityElement.dataset.identificator;
 
-                    const cityObject = new CityObject(voivodeId, cityId, timeHotelArr, clientRouteId);
+                    const cityObject = new CityObject(voivodeId, cityId, timeHotelArr, clientRouteId,priceHotelArr);
                     if(isOk != false) {
                         isOk = cityObject.validate();
                     }
@@ -456,11 +496,16 @@
 
                     if(attributesOfClickedElement.getNamedItem('type')) {
                         let typeAttribute = attributesOfClickedElement.getNamedItem('type').nodeValue;
-                        if(typeAttribute == 'search') {
+                        console.log(typeAttribute);
+                        if(typeAttribute == 'search' || typeAttribute == 'number') {
                             const clickedInput = e.target;
                             const clickedInputValue = clickedInput.value;
-                            const wholeContainer = clickedInput.parentElement.parentElement.parentElement.parentElement;
-                            const allSearchBars = wholeContainer.querySelectorAll('[type="search"]');
+                            if(typeAttribute == 'search')
+                                var wholeContainer = clickedInput.parentElement.parentElement.parentElement.parentElement;
+                            else
+                                var wholeContainer = clickedInput.parentElement.parentElement;
+                            console.log(wholeContainer);
+                            const allSearchBars = wholeContainer.querySelectorAll('[type='+typeAttribute+']');
 
                             let flag = false;
                             allSearchBars.forEach(bar => {
@@ -472,7 +517,6 @@
                                     bar.value = clickedInputValue;
                                 }
                             });
-
                         }
                     }
 
