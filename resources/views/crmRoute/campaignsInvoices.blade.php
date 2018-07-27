64f92d6b76bcd721bd1122bfd79fe9c613ae0cd4
Sebastian <skobry123on@gmail.com>
2018-07-27 08:21:39 +0200

    raload table before send mail

--- resources/views/crmRoute/campaignsInvoices.blade.php
+++ resources/views/crmRoute/campaignsInvoices.blade.php
@@ -528,7 +528,7 @@
                                actualCampaignID: actualCampaignID
                            },
                            success: function (response) {
-                               console.log(response);
+                               invoiceDatatable.ajax.reload();
                                myModal.modal('hide');
                            } 
                         });
