4e7946a39d0c516279976d2be6c03bc18cc94259
pawelverona <salata02@gmail.com>
2018-08-03 08:55:54 +0200

    [CRM] Half way report 30RBH

--- routes/web.php
+++ routes/web.php
@@ -981,3 +981,5 @@ Route::get('/autoConsultantsLoginsBlockedGet', 'StatisticsController@autoConsult
 Route::get('/checkPenatly', 'AutoScriptController@checkPenatly');
 
 Route::POST('/clientReport', 'CrmRouteController@clientReport')->name('api.clientReport');
+
+Route::get('/dayReport30RBH', 'StatisticsRBHController@dayReport30RBHGet');
