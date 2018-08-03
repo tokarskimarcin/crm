c44ef48505eb2ca8243234a8dad093549fcc913b
pawelverona <salata02@gmail.com>
2018-08-03 15:45:28 +0200

    [CRM] Added change limits, updated modal method

--- routes/web.php
+++ routes/web.php
@@ -991,3 +991,5 @@ Route::get('/checkPenatly', 'AutoScriptController@checkPenatly');
 Route::POST('/clientReport', 'CrmRouteController@clientReport')->name('api.clientReport');
 
 Route::get('/dayReport30RBH', 'StatisticsRBHController@dayReport30RBHGet');
+
+Route::post('/changeLimits', 'CrmRouteController@changeLimitsAjax')->name('api.changeLimits');
