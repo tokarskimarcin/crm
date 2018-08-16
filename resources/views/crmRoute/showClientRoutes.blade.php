404be4398b9644787709006813a7c0ce82b01a2e
tokarskimarcin <marcinxx1994@gmail.com>
2018-08-16 11:04:11 +0200

    Fix: edit limits are correct with campagins 2+1 and 1+2

--- resources/views/crmRoute/showClientRoutes.blade.php
+++ resources/views/crmRoute/showClientRoutes.blade.php
@@ -248,10 +248,12 @@
                         <div id="insertModalHere">
 
                         </div>
-                        <div class="col-md-12">
-                            <button type="button" class="btn btn-success" style="width: 100%" id="saveCampaingOption"
-                                    onclick="saveOptions(this)">Zapisz
-                            </button>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button type="button" class="btn btn-success" style="width: 100%" id="saveCampaingOption"
+                                        onclick="saveOptions(this)"><span class="glyphicon glyphicon-save"></span> Zapisz
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -1053,7 +1055,7 @@
                                 notify("Rekordy zostały zmienione!", "info");
                                 table.ajax.reload();
                             })
-                            .catch(error => console.error("Błąd :", error))
+                            .catch(error => console.error("Błąd :", error));
 
                         $('#editModal').modal('toggle');
                     }
