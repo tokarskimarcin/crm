a3a25fba0f511bd80f7114d7cf1cbd027b54b2b1
Sebastian <skobry123on@gmail.com>
2018-08-21 09:22:07 +0200

    Change method count limit

--- app/Http/Controllers/CrmRouteController.php
+++ app/Http/Controllers/CrmRouteController.php
@@ -3064,7 +3064,7 @@ class CrmRouteController extends Controller
                         };
                         $limit = $eventLimit;
                     }
-                    $arrayResult['sumOfLimits'] = round($limit/$departmentInfo->count(),2) ;
+                    $arrayResult['sumOfLimits'] = $limit;
                     $arrayResult['sumOfActualSuccess'] = 0;
 
                     $merge = $routeInfoOverall->where('date',$itemDate)
@@ -3079,6 +3079,7 @@ class CrmRouteController extends Controller
                                     ->where('department_info_id',null)->first()['sumOfLimits'] += round($arrayResult['sumOfLimits']/count($departmentInfo),2);
                         }
                     }else{
+                        $arrayResult['sumOfLimits'] = round(arrayResult['sumOfLimits']/$departmentInfo->count(),2) ;
                         $routeInfoOverall->push(collect($arrayResult));
                     }
                 }
