7021c7ea5834aa3897681a9b582a189209563d62
pawelverona <salata02@gmail.com>
2018-08-29 15:26:43 +0200

    [SCHEDULES] Created new checkbox for paid das

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -278,6 +278,13 @@ class ScheduleController extends Controller
 
     public function saveSchedule(Request $request)
     {
+        if(isset($request->isPaid)) {
+            $paid = $request->isPaid;
+        }
+        else {
+            $paid = ['true', 'true', 'true', 'true', 'true', 'true', 'true'];
+        }
+
         $start_hours = $request->start_hours;
         $stop_hours = $request->stop_hours;
         $reasons = $request->reasons;
@@ -321,30 +328,37 @@ class ScheduleController extends Controller
             $schedule->monday_start = $start_hours[0];
             $schedule->monday_stop = $stop_hours[0];
             $schedule->monday_comment =  $reasons[0];
+            $schedule->mondayPaid =  $paid[0] == 'false' ? 0 : 1;
 
             $schedule->tuesday_start = $start_hours[1];
             $schedule->tuesday_stop = $stop_hours[1];
             $schedule->tuesday_comment =  $reasons[1];
+            $schedule->tuesdayPaid =  $paid[1] == 'false' ? 0 : 1;
 
             $schedule->wednesday_start = $start_hours[2];
             $schedule->wednesday_stop = $stop_hours[2];
             $schedule->wednesday_comment =  $reasons[2];
+            $schedule->wednesdayPaid =  $paid[2] == 'false' ? 0 : 1;
 
             $schedule->thursday_start = $start_hours[3];
             $schedule->thursday_stop = $stop_hours[3];
             $schedule->thursday_comment =  $reasons[3];
+            $schedule->thursdayPaid =  $paid[3] == 'false' ? 0 : 1;
 
             $schedule->friday_start = $start_hours[4];
             $schedule->friday_stop = $stop_hours[4];
             $schedule->friday_comment =  $reasons[4];
+            $schedule->fridayPaid =  $paid[4] == 'false' ? 0 : 1;
 
             $schedule->saturday_start = $start_hours[5];
             $schedule->saturday_stop = $stop_hours[5];
             $schedule->saturday_comment =  $reasons[5];
+            $schedule->saturdayPaid =  $paid[5] == 'false' ? 0 : 1;
 
             $schedule->sunday_start = $start_hours[6];
             $schedule->sunday_stop = $stop_hours[6];
             $schedule->sunday_comment =  $reasons[6];
+            $schedule->sundayPaid = $paid[6] == 'false' ? 0 : 1;
 
             $schedule->save();
 
