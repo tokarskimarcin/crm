649e2c1bfa32f8cfe5737b5a1914ec1bd6fbe356
Sebastian <skobry123on@gmail.com>
2018-08-31 13:02:19 +0200

    End consultant month report [30 RBH]

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4770,7 +4770,8 @@ public function getCoachingDataAllLevel($month, $year, $dep_id,$level_coaching,$
                 'coaches'   => $coaches,
                 'months'    => self::getMonthsNames(),
                 'month'     => date('m'),
-                'coach_selected' => 0
+                'coach_selected' => 0,
+                'onlyNewUser'   => 0,
             ]);
     }
 
@@ -4790,14 +4791,19 @@ public function getCoachingDataAllLevel($month, $year, $dep_id,$level_coaching,$
         $date_stop = date('Y-') . $request->month_selected . date('-t', strtotime(date('Y-') . $request->month_selected)) ;
 
         $data = self::monthReportConsultantsData($request->coach_id, $date_start, $date_stop);
+        $newUserID = [];
+        if($request->onlyNewUser == 1)
+            $newUserID = $this::getUserLessThan30RBH()->pluck('id_user')->toArray();
 
         return view('reportpage.monthReportConsultant')
             ->with([
-                'coaches'   => $coaches,
-                'months'    => self::getMonthsNames(),
-                'month'     => $request->month_selected,
-                'coach_selected' => $request->coach_id,
-                'data'      => $data
+                'coaches'       => $coaches,
+                'months'        => self::getMonthsNames(),
+                'month'         => $request->month_selected,
+                'coach_selected'=> $request->coach_id,
+                'data'          => $data,
+                'onlyNewUser'   => $request->onlyNewUser,
+                'onlyUserID'    => $newUserID
             ]);
     }
 
