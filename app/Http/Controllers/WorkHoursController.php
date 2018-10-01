b66b4bd8712929279e66e86ac46f28a75d6ae4b9
tokarskimarcin <marcinxx1994@gmail.com>
2018-10-01 13:23:15 +0200

    getUserTypesPermissionToEditSuccess

--- app/Http/Controllers/WorkHoursController.php
+++ app/Http/Controllers/WorkHoursController.php
@@ -24,12 +24,15 @@ class WorkHoursController extends Controller
     }
 
 
+    public static function getUserTypesPermissionToEditSuccess(){
+        return [3,23];
+    }
     //******************acceptHour****************** START
     public function acceptHour()
     {
         $myDepartment_info = Department_info::find(Auth::user()->department_info_id);
         $count_agreement = Department_types::find($myDepartment_info->id_dep_type);
-        if($count_agreement->count_agreement == 1) // czy zliczane są zagody
+        if($count_agreement->count_agreement == 1 && in_array(Auth::user()->user_type_id,WorkHoursController::getUserTypesPermissionToEditSuccess())) // czy zliczane są zagody
         {
             return view('workhours.acceptHourSucces');
         }
