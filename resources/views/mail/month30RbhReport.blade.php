62de63af7c37e261e1bfdf87c252864b1e00447f
pawelWritesCode <p.t.chmielewski@gmail.com>
2018-10-15 08:14:51 +0200

    [REPORT] Changed layout of report

--- resources/views/mail/month30RbhReport.blade.php
+++ resources/views/mail/month30RbhReport.blade.php
@@ -1,9 +1,9 @@
 <table style="width:100%;border:1px solid #231f20;border-collapse:collapse;padding:3px">
     <thead style="color:#efd88f">
     <tr>
-        <td colspan="2" style="border:1px solid #231f20;text-align:center;padding:3px;background:#231f20;color:#efd88f; width: 40%;">
+        <td colspan="1" style="border:1px solid #231f20;text-align:center;padding:3px;background:#231f20;color:#efd88f; width: 40%;">
             <font size="6" face="Calibri">Miesięczny 30 Rbh(zbiorczy) {{$date_start}} - {{$date_stop}}</font></td>
-        <td colspan="2" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20">
+        <td colspan="3" style="border:1px solid #231f20;text-align:left;padding:6px;background:#231f20">
             <img src="http://teambox.pl/image/logovc.png" class="CToWUd"></td>
     </tr>
     <tr>
